218fa79f233f2d16f1c2538c9a9de491ed38cc0a
mohankumar <ntrmohan837@gmail.com>
2024-01-11 15:23:17 +0530

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,17 @@ import { CustomerComponent } from './customer/customer.component';
 import { OrdersComponent } from './orders/orders.component';
 import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+
 
 const routes: Routes = [
-  { path: '', component: CustomerComponent },
+  { path: '', component: WelcomeComponent },
+  { path: 'customers', component: CustomerComponent },
   { path: 'orders', component: OrdersComponent },
   { path: 'about', component: AboutComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'welcome', component: WelcomeComponent  },
+  
 ];
 
 @NgModule({
